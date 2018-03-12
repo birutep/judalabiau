9e1bcd6dec0e82e1b31e4714f04218ace809ce30
Daniel Rozov <danielrozov@gmail.com>
2018-03-12 12:38:24 +0200

    Modified pom.xml files, Added new classes: Book, Author

---
+++ BookStore/src/main/java/lt/judalabiau/BookStore/models/Author.java
@@ -0,0 +1,92 @@
+package lt.judalabiau.BookStore.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Author {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column
+	private String name;
+
+	@Column
+	private String surname;
+
+	@Column
+	private Date birthDate;
+
+	@Column
+	private Date deathDate;
+
+	@Column
+	private String biography;
+
+	public Author() {
+		super();
+	}
+
+	public Author(String name, String surname, Date birthDate, Date deathDate, String biography) {
+		this.name = name;
+		this.surname = surname;
+		this.birthDate = birthDate;
+		this.deathDate = deathDate;
+		this.biography = biography;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Date getDeathDate() {
+		return deathDate;
+	}
+
+	public void setDeathDate(Date deathDate) {
+		this.deathDate = deathDate;
+	}
+
+	public String getBiography() {
+		return biography;
+	}
+
+	public void setBiography(String biography) {
+		this.biography = biography;
+	}
+}
---
+++ BookStore/src/main/java/lt/judalabiau/BookStore/models/Book.java
@@ -0,0 +1,49 @@
+package lt.judalabiau.BookStore.models;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column
+	private String title;
+
+	@Column
+	private Date releaseDate;
+
+	@Column
+	private int isbn;
+
+	@Column
+	private BigDecimal price;
+
+	@Column
+	private String description;
+	
+	@Column
+	private String photopath;
+	
+	@Column
+	private int count;
+	
+	@Column
+	private double rating; //Count stars
+
+	@Column
+	private double ratingCount; //How many voters voted
+
+	
+	
+	
+}
