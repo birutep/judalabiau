78371bc12c3eee7703ee496f7c31d3f91ff4fa22
Aurimas Cincikas <cincikas.aurimas.jp17@gmail.com>
2018-03-13 20:49:53 +0200

    isvalytos react siuksles, pakeista config automatizuojanti css klasiu pavadinimus, pridetas wraperis kad isventk bereikalingu divu

--- bookstore-front/src/App.js
+++ bookstore-front/src/App.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import BookRegForm from './BookRegForm';
+import BookRegForm from './components/book_reg_form/BookRegForm';
+import Wrap from './components/wrap/wrap'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+      <Wrap>
             <BookRegForm/>
-        </p>
-      </div>
+      </Wrap>
     );
   }
 }
