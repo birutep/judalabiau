8b2dd4e71babba566376f223f38f6d922e934d79
Daniel Rozov <danielrozov@gmail.com>
2018-03-31 13:47:37 +0300

    Create a new package user_reg_form. Create a new file CustomerRegForm.js

---
+++ bookstore-front/src/components/user_reg_form/customerRegForm.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { InputText } from '../../../node_modules/primereact/components/inputtext';
+import 'primereact/resources/primereact.min.css';
+import { Button } from '../../../node_modules/primereact/components/button';
+
+export class CustomerRegForm extends Component {
+    
+}
