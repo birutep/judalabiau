1f259d89b0f1e13d111f84f06734d3c096d10689
Aurimas Cincikas <cincikas.aurimas.jp17@gmail.com>
2018-04-07 03:23:37 +0300

    prideta paprasti pavyzdziai prime faces knygu lentelems atvaizduoti

--- bookstore-front/src/index.js
+++ bookstore-front/src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "mobx-react";
-//primereact style
-import "primereact/resources/primereact.min.css";
-import "primereact/resources/themes/omega/theme.scss";
-import "font-awesome/css/font-awesome.css";
-//custom style
-import "./App.css"
 //custom elements
 import BookStore from "./stores/BookStore";
 import App from "./App";
