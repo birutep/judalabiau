2e45581cb16d81a29faab667fc4d3274591a3130
Aurimas Cincikas <cincikas.aurimas.jp17@gmail.com>
2018-04-17 20:20:59 +0300

    pridetas delete ir update servisas

--- BookStore/src/main/java/lt/judalabiau/BookStore/users/UserService.java
+++ BookStore/src/main/java/lt/judalabiau/BookStore/users/UserService.java
@@ -28,26 +28,32 @@ public class UserService {
     }
 
     @Transactional
-    public void createUser(UserDTO userDTO){
+    public UserDTO createUser(UserDTO userDTO){
         if(userRepository.existsByEmail(userDTO.getEmail())){
             //veliau mesim exception
-            System.out.println("Toks useris jau yra----------------");
+            System.out.println("Toks useris jau yra");
+            return null;
         }else{
+            User saved = null;
             switch (userDTO.getRole().intValue()){
                 case 1:
-                    userRepository.save( converterToAdmin.convert(userDTO));
+                    saved = userRepository.save( converterToAdmin.convert(userDTO));
                     break;
                 case 2:
-                    userRepository.save(converterToSalesman.convert(userDTO));
+                    saved = userRepository.save(converterToSalesman.convert(userDTO));
                     break;
                 case 3:
-                    userRepository.save( converterToCustomer.convert(userDTO));
+                    saved = userRepository.save( converterToCustomer.convert(userDTO));
                     break;
                 default:
                     //gal mesim exceptiona
                      System.out.println("Tokios roles nera");
                      break;
             }
+            if(saved!=null){
+                return converterToDTO.convert(saved);
+            }
+            return null;
         }
     }
 
@@ -60,6 +66,36 @@ public class UserService {
         return  users;
     }
 
+    @Transactional
+    public void deleteUser(Long id){
+        userRepository.deleteById(id);
+    }
+
+    @Transactional
+    public UserDTO updateUser(Long id, UserDTO dto){
+        User userToUpdate = userRepository.findById(id).orElse(null);
+        if(userToUpdate==null){
+            System.out.println("Tokio tokiu id nera, netuyrime ka koreguoti");
+            return null;
+        }
+        User userPassedByDTO = null;
+        switch (dto.getRole().intValue()){
+            case 1:userPassedByDTO=converterToAdmin.convert(dto); break;
+            case 2:userPassedByDTO=converterToSalesman.convert(dto); break;
+            case 3:userPassedByDTO=converterToCustomer.convert(dto); break;
+            default:
+                System.out.println("Nepavyko atnaujinti vartotojo, tokiso roles nera");        return null;
+        }
+        if(userPassedByDTO!=null){
+            userPassedByDTO.setId(userToUpdate.getId());//id nekeiciamas, todel tiesiog perasom ta kuris buvo originalus
+            User updated = userRepository.save(userPassedByDTO);    //issaugojimo i repozitorija rezultatas, kuris konvertuojamas atgal i dto kaip responsas
+            return converterToDTO.convert(updated);
+        }
+        return null;
+    }
+
+
+    //naudojamas tik init pradine duombaze, jei neatsiras poreikio, buildinant pasalint
     @Transactional
     public void saveAll(Iterable<User> users){
         userRepository.saveAll(users);
