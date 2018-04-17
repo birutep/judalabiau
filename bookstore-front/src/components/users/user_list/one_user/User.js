e3782e286d3348e261bb79dfafc7263602781d7d
Daniel Rozov <danielrozov@gmail.com>
2018-04-17 06:28:44 +0300

    Modified UserList. Added <SubHeader/> and <th/> <th/>. Modified User.js. Added trash icon

--- bookstore-front/src/components/users/user_list/one_user/User.js
+++ bookstore-front/src/components/users/user_list/one_user/User.js
@@ -8,7 +8,21 @@ const User = props => {
             <td>{props.singleUser.fName}</td>
             <td>{props.singleUser.lName}</td>
             <td>{props.singleUser.email}</td>
-            <td>{props.singleUser.phone!=0?props.singleUser.phone:'-'}</td>
+            <td>
+                {props.singleUser.phone != 0 ? props.singleUser.phone : "-"}
+            </td>
+            {/* <td className="mini">
+                <Link to="/books/edit">
+                    <i
+                        className="fa fa-pencil fa-fw"
+                        onClick={() => this.editBook(this.props.myBook)}
+                    />
+                </Link>
+            </td> */}
+            <td className="mini">
+                {/* <i className="fa fa-trash-o fa-fw" onClick={this.deleteBook} /> */}
+                <i className="fa fa-trash-o fa-fw" />
+            </td>
         </tr>
     );
 };
