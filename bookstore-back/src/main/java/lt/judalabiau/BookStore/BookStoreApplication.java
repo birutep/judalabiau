6277e5cb1671cc7cb04fdfc935951da684384ae0
Aurimas Cincikas <cincikas.aurimas.jp17@gmail.com>
2018-04-19 13:08:10 +0300

    duombaziu uzpildymas is sql, pasalintos klases anksciau kurusios pradinius duomenis ir juos irasinejancios i duombaze, programai testuoti

--- bookstore-back/src/main/java/lt/judalabiau/BookStore/BookStoreApplication.java
+++ bookstore-back/src/main/java/lt/judalabiau/BookStore/BookStoreApplication.java
@@ -1,5 +1,13 @@
 package lt.judalabiau.BookStore;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lt.judalabiau.BookStore.books.Book;
+import lt.judalabiau.BookStore.books.BookService;
+import lt.judalabiau.BookStore.books.dto.BookDTO;
+import lt.judalabiau.BookStore.books.dto.converters.BookToDTO;
+import lt.judalabiau.BookStore.users.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +17,11 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableSwagger2
 @SpringBootApplication
