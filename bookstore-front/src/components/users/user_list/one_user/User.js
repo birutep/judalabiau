6a527d9e8a15d83fc269828ec5acd7ff0fb0734d
Daniel Rozov <danielrozov@gmail.com>
2018-04-21 15:11:47 +0300

    Added latest version from master

--- bookstore-front/src/components/users/user_list/one_user/User.js
+++ bookstore-front/src/components/users/user_list/one_user/User.js
@@ -6,55 +6,64 @@ import { Link } from "react-router-dom";
 import "font-awesome/css/font-awesome.min.css";
 
 class User extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userEdit: {
-        fName: props.singleUser.fName,
-        lName: props.singleUser.lName,
-        email: props.singleUser.email,
-        phone: props.singleUser.phone
-      }
+    constructor(props) {
+        super(props);
+        this.state = {
+            userEdit: {
+                fName: props.singleUser.fName,
+                lName: props.singleUser.lName,
+                email: props.singleUser.email,
+                phone: props.singleUser.phone
+            }
+        };
+    }
+
+    deleteUser = e => {
+        e.preventDefault();
+        console.log("Vartotojas ištrintas");
+        axios.delete(USERS + this.props.singleUser.id);
     };
-  }
 
-  deleteUser = e => {
-    e.preventDefault();
-     axios.delete(USERS + this.props.singleUser.id).then(() => {
-         this.props.BookStore.changeState();
-     });
-  };
+    editUser = user => {
+        this.props.BookStore.editUser(user);
+        this.props.BookStore.changeState();
+    };
 
-  editUser = user => {
-    this.props.BookStore.editUser(user);
-    this.props.BookStore.changeState();
-  };
+    render() {
+        return (
+            <tr>
+                <td>{this.props.singleUser.fName}</td>
+                <td>{this.props.singleUser.lName}</td>
+                <td>{this.props.singleUser.email}</td>
+                <td>
+                    {this.props.singleUser.phone !== 0
+                        ? this.props.singleUser.phone
+                        : "-"}
+                </td>
+                <td className="mini">
+                    <Link to="users/edit">
+                        <i
+                            className="fa fa-pencil fa-fw"
+                            onClick={() => this.editUser(this.props.singleUser)}
+                        />
+                    </Link>
+                </td>
 
-  render() {
-    return (
-      <tr>
-        <td>{this.props.singleUser.fName}</td>
-        <td>{this.props.singleUser.lName}</td>
-        <td>{this.props.singleUser.email}</td>
-        <td>
-          {this.props.singleUser.phone !== 0
-            ? this.props.singleUser.phone
-            : "-"}
-        </td>
-        <td className="mini">
-          <Link to="users/edit">
-            <i
-              className="fa fa-pencil fa-fw"
-              onClick={() => this.editUser(this.props.singleUser)}
-            />
-          </Link>
-        </td>
-        <td className="mini">
-          <i className="fa fa-trash-o fa-fw" onClick={this.deleteUser} />
-        </td>
-      </tr>
-    );
-  }
+                <td className="mini">
+                    <i
+                        className="fa fa-trash-o fa-fw"
+                        onClick={this.deleteUser}
+                    />
+                    {/* <Button
+                        iconPos="right"
+                        icon="fa fa-trash-o fa-fw"
+                        onClick={this.deleteUser}
+                    /> */}
+                   
+                </td>
+            </tr>
+        );
+    }
 }
 
 export default User;
